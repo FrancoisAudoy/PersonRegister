c980ffc44f4b5a97c25873a62f882b22270b641d
FrancoisAudoy <faudoy@u-bordeaux.fr>
2022-08-07 11:59:05 +0200

    Init test

---
+++ src/test/java/fr/francoisaudoy/PersonRegister/controller/RegisterControllerTest.java
@@ -0,0 +1,19 @@
+package fr.francoisaudoy.PersonRegister.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class RegisterControllerTest {
+
+    @Autowired
+    private RegisterController controller;
+
+    @Test
+    void findPersonInformationSuccess() {
+
+    }
+
+    @Test
+    void addPersonSuccess() {
+    }
+}
