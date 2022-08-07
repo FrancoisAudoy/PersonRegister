072dc53c30ad9b5b3eea6a4799e058c0b8055fd4
FrancoisAudoy <faudoy@u-bordeaux.fr>
2022-08-07 18:17:49 +0200

    Controller add user

--- src/main/java/fr/francoisaudoy/personregister/controller/RegisterController.java
+++ src/main/java/fr/francoisaudoy/personregister/controller/RegisterController.java
@@ -1,6 +1,10 @@
 package fr.francoisaudoy.personregister.controller;
 
-import lombok.NoArgsConstructor;
+import fr.francoisaudoy.personregister.model.PersonDto;
+import fr.francoisaudoy.personregister.model.entity.PersonEntity;
+import fr.francoisaudoy.personregister.service.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/person")
 public class RegisterController {
 
+    @Autowired
+    private RegisterService registerService;
+
     @GetMapping(value = "{personId}/information",
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> findPersonInformation(@PathVariable(name = "personId") String id) {
@@ -19,7 +26,8 @@ public class RegisterController {
 
     @PutMapping(value = "/register",
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> addPerson() {
-        return null;
+    public ResponseEntity<String> addPerson(@RequestBody PersonDto person) {
+        PersonEntity entity = registerService.createPerson(person);
+        return ResponseEntity.ok(entity.toString());
     }
 }
