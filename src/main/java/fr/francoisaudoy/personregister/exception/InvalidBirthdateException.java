cff65255ce05e8ca540deb0e19fa308fd2daaf85
FrancoisAudoy <francois.audoy@gmail.com>
2022-08-11 19:17:37 +0200

    Better error status

--- src/main/java/fr/francoisaudoy/personregister/exception/InvalidBirthdateException.java
+++ src/main/java/fr/francoisaudoy/personregister/exception/InvalidBirthdateException.java
@@ -1,6 +1,10 @@
 package fr.francoisaudoy.personregister.exception;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class InvalidBirthdateException extends PersonRegisterException {
 
     public InvalidBirthdateException(String msg) {
